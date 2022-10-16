0c0318948bc793ed33024579065157bd7035809d
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-16 19:46:41 +0200

    add: userdefault

--- SwiftUIWidgetExample/ContentView.swift
+++ SwiftUIWidgetExample/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State var inputText: String = ""
+	@State var displayText: String = ""
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+			VStack(alignment: .leading) {
+					
+					Text("Input Text: \(self.displayText)")
+					
+					TextField("Type something...", text: $inputText)
+					
+					Button("Save", action: {
+						UserDefaults.standard.setValue(self.inputText, forKey: "text")
+						self.displayText = self.inputText
+						self.inputText = ""
+					})
+					
         }
         .padding()
     }
