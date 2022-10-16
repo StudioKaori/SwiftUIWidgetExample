3301b405f340d5747c971e3f397a37c59031808c
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-16 19:28:47 +0200

    Initial Commit

---
+++ SwiftUIWidgetExample/SwiftUIWidgetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWidgetExampleApp.swift
+//  SwiftUIWidgetExample
+//
+//  Created by Kaori Persson on 2022-10-16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWidgetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
