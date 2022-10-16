4812d7a015e913e471ff82b3c36f94fa80d6fd21
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-16 19:55:28 +0200

    Add: text to the widget

--- TextWidget/TextWidget.swift
+++ TextWidget/TextWidget.swift
@@ -11,22 +11,27 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+			SimpleEntry(date: Date(), text: "", configuration: ConfigurationIntent())
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        let entry = SimpleEntry(date: Date(), text: "", configuration: configuration)
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
+			
+			let userDefaults = UserDefaults(suiteName: "textWidgetCache")
+			let text = userDefaults?.value(forKey: "text") as? String ?? "No text"
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            let entry = SimpleEntry(date: entryDate,
+																		text: text,
+																		configuration: configuration)
             entries.append(entry)
         }
 
@@ -37,6 +42,7 @@ struct Provider: IntentTimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+	let text: String
     let configuration: ConfigurationIntent
 }
 
@@ -49,7 +55,9 @@ struct TextWidgetEntryView : View {
 					.resizable()
 					.aspectRatio(contentMode: .fill)
 				
-				Text(entry.date, style: .time)
+				Text(entry.text)
+					.font(.system(size: 30))
+					.foregroundColor(.white)
 			}
     }
 }
@@ -69,7 +77,7 @@ struct TextWidget: Widget {
 
 struct TextWidget_Previews: PreviewProvider {
     static var previews: some View {
-        TextWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        TextWidgetEntryView(entry: SimpleEntry(date: Date(), text: "Hello widget", configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
