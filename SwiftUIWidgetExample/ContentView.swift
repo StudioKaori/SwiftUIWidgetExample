1cc8f8543435c46bd9c873bd137a751203216894
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-16 19:47:27 +0200

    Style

--- SwiftUIWidgetExample/ContentView.swift
+++ SwiftUIWidgetExample/ContentView.swift
@@ -11,26 +11,28 @@ struct ContentView: View {
 	@State var inputText: String = ""
 	@State var displayText: String = ""
 	
-    var body: some View {
-			VStack(alignment: .leading) {
-					
-					Text("Input Text: \(self.displayText)")
-					
-					TextField("Type something...", text: $inputText)
-					
-					Button("Save", action: {
-						UserDefaults.standard.setValue(self.inputText, forKey: "text")
-						self.displayText = self.inputText
-						self.inputText = ""
-					})
-					
-        }
-        .padding()
-    }
+	var body: some View {
+		VStack(alignment: .leading) {
+			
+			Text("Input Text: \(self.displayText)")
+			
+			Spacer()
+			
+			TextField("Type something...", text: $inputText)
+			
+			Button("Save", action: {
+				UserDefaults.standard.setValue(self.inputText, forKey: "text")
+				self.displayText = self.inputText
+				self.inputText = ""
+			})
+			
+		}
+		.padding()
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
